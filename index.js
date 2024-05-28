4eb29cc7f979dae12d7d103a37ca4ee211a9a0c2
twinkle-tripti7 <139100556+twinkle-tripti7@users.noreply.github.com>
2024-05-29 00:46:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Portfolio_Components/Header';
+import './index.css';
+ReactDOM.render(<>
+    
+    <Header/>
+    </>,
+    document.getElementById("root")
+);
